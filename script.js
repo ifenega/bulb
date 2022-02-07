a16de926b4c6b551887dfda587d7148d2a416ecc
ifenega <69639702+ifenega@users.noreply.github.com>
2022-02-07 01:09:53 +0100

    i added the toggle section

---
+++ script.js
@@ -0,0 +1,16 @@
+
+const toggle = document.querySelector('.toggle')
+const navigation = document.querySelector('.navigation')
+const img = document.querySelector('.fam');
+const img2 = document.querySelector('.yam');
+console.log (img.classList)
+
+toggle.addEventListener('click', () => {
+  toggle.classList.toggle('active')
+  navigation.classList.toggle('active')
+  img.classList.toggle('active')
+  img2.classList.toggle('active')
+
+  console.log(toggle.classList)
+  console.log(navigation.classList)
+})
